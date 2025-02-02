c11bdaab128d5a014b81555db08dd4d8c7a9bd37
Jackie Yang <yang192827@163.com>
2025-02-03 01:53:46 +0800

    修改布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,9 @@ import PasswordGenerator from '@/components/PasswordGenerator.vue'
 </script>
 
 <template>
-  <main class="!flex min-h-screen !items-center !justify-center bg-gray-100">
+  <main class="!flex h-dvh !items-center !justify-center bg-gray-100">
     <div class="w-full max-w-lg px-8">
-      <PasswordGenerator />
+      <password-generator />
     </div>
   </main>
 </template>
